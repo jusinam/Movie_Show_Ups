347180aaf36b7805f491343e3bb936117f21b52f
DjCooGie <evansonchagwa01@gmail.com>
2019-12-29 18:21:42 +0300

    modified files

--- app/main/views.py
+++ app/main/views.py
@@ -11,25 +11,22 @@ from ..models import Review
 # Views
 @main.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
-
     # Getting popular movie
     popular_movies = get_movies('popular')
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
-
     title = 'Home - Welcome to The best Movie Review Website Online'
-
+    
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('.search', movie_name=search_movie))
     else:
-        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
-
+    
+        return render_template('index.html', title=title, popular=popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie)
 
 @main.route('/movie/<int:id>')
 def movie(id):
